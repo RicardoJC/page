c588c1c04b673987c6b4b67a23031ed5088280cb
Ricardo Jiménez <ricardojimenez@Richter.local>
2020-10-26 08:49:36 -0600

    Updates

---
+++ precache-manifest.976e0041c5f70ddcfde75d0d23a10da6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5829acb4345d2515d7d0e67aef24b44e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ee18ede27e637404a38",
+    "url": "/static/css/main.a6e395d1.chunk.css"
+  },
+  {
+    "revision": "84fac91fa719e79a3a1b",
+    "url": "/static/js/2.709f8339.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.709f8339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ee18ede27e637404a38",
+    "url": "/static/js/main.e1dd6716.chunk.js"
+  },
+  {
+    "revision": "a4c54faf1d2231e197ef",
+    "url": "/static/js/runtime-main.bf3ed384.js"
+  }
+]);
